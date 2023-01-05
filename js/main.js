518b10567dd7ad3e427f5c36bf0a9732992a3759
Shevchenko Andrey <demorest49de@gmail.com>
2023-01-05 22:39:07 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+{
+  const createContainer = () => {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    return container;
+  };
+
+  const createHeader = () => {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const headerContainer = createContainer();
+    header.append(headerContainer);
+
+    header.headerContainer = headerContainer;
+
+    return header;
+  };
+
+  const createLogo = (title) => {
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = `Телефонный справочник. ${title}`;
+    return h1;
+  };
+
+  const createMain = () => {
+    const main = document.createElement('main');
+    const mainContainer = createContainer();
+    main.append(mainContainer);
+    main.mainContainer = mainContainer;
+    return main;
+  };
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const header = createHeader();
+    const logo = createLogo(title);
+    header.headerContainer.append(logo);
+    app.append(header);
+  };
+
+  window.phoneBookInit = init;
+}

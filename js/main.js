6931db30482a0197f4a45d0c18c3b2d13142f317
Shevchenko Andrey <demorest49de@gmail.com>
2023-01-23 12:32:48 +0300

    main.js changed;

--- js/main.js
+++ js/main.js
@@ -347,8 +347,8 @@
       const dirModifier = sortSwitch ? 1 : -1;
       const rows = [...list.childNodes];
       const sortedRows = rows.sort((a, b) => {
-        const aText = a.childNodes[column].textContent;
-        const bText = b.childNodes[column].textContent;
+        const aText = a.childNodes[column].textContent.trim();
+        const bText = b.childNodes[column].textContent.trim();
 
         return aText > bText ? (1 * dirModifier) : (-1 * dirModifier);
       });
